84c83155253251a56aca1b3eb16ec6b53d89fbc5
Sean Kim <seandkim14@gmail.com>
2017-11-13 13:45:41 -0500

    gets the raw data

--- data/processData.py
+++ data/processData.py
@@ -1,1 +1,19 @@
-print('hello')
+import os
+
+def readFile(filename): 
+	with open(filename, 'r') as fp: 
+		lines = fp.read()
+		return lines
+
+def getData(dir): 
+	data = dict()
+	for filename in os.listdir(dir):
+		if (filename[0] != '.' and filename[0] != '_'): 
+			data[filename] = readFile(dir + filename)
+
+	return data
+
+
+DATA_DIR = "results/"
+data = getData(DATA_DIR)
+print(data)
